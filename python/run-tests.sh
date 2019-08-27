f4d1aa79c47700a173e0e1a4fd7cdfd88152e6ad
Chia-Che Tsai <chiache@tamu.edu>
2019-08-27 11:31:30 -0500

    fixup! [Apps] Rewrite Python example

---
+++ python/run-tests.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+echo "\n\nBuilding Python..."
+make >> /dev/null 2>&1
+
+echo "\n\nRun helloworld.py:"
+./pal_loader python.manifest scripts/helloworld.py > OUTPUT
+grep -q "Hello World" OUTPUT && echo "[ SUCCESS ]"
+rm OUTPUT
+
+echo "\n\nRun fibonacci.py:"
+./pal_loader python.manifest scripts/fibonacci.py  > OUTPUT
+grep -q "fib2              55" OUTPUT && echo "[ SUCCESS ]"
+rm OUTPUT
+
+echo "\n\nRun test-numpy.py:"
+./pal_loader python.manifest scripts/test-numpy.py > OUTPUT
+grep -q "maxint: " OUTPUT && echo "[ SUCCESS ]"
+cat OUTPUT
+rm OUTPUT
+
+echo "\n\nRun test-scipy.py:"
+./pal_loader python.manifest scripts/test-scipy.py > OUTPUT
+grep -q "cholesky: " OUTPUT && echo "[ SUCCESS ]"
+cat OUTPUT
+rm OUTPUT
+
+./web-test.sh
--- python/web-test.sh
+++ python/web-test.sh
@@ -3,12 +3,13 @@
 ## We really need to pick a unique ephemeral port; start by just picking pid+1024
 PORT=$(($$ + 1024))
 
-echo "\n\nRun a HTTP server in the background on port " + $PORT
+echo -e "\n\nRun a HTTP server in the background on port " + $PORT
 python3 scripts/dummy-web-server.py $PORT & echo $! > server.PID
 sleep 1
-echo "\n\nRun test-http.py:"
+
+echo -e "\n\nRun test-http.py:"
 ./pal_loader python.manifest scripts/test-http.py 127.0.0.1 $PORT > OUTPUT1
 wget -q http://127.0.0.1:$PORT/ -O OUTPUT2
-diff -q OUTPUT1 OUTPUT2 || exit $?
+diff -q OUTPUT1 OUTPUT2 && echo "[ SUCCESS ]" || exit $?
 kill `cat server.PID`
 rm -f OUTPUT1 OUTPUT2 server.PID
