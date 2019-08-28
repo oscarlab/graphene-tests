c988312436747440aeed255c6f5ead57031ab562
Wojtek Porczyk <woju@invisiblethingslab.com>
2019-08-28 20:19:11 +0200

    fixup! [LTP]: report results as junit

--- ltp/runltp_xml.py
+++ ltp/runltp_xml.py
@@ -18,9 +18,9 @@
 #
 
 import argparse
+import asyncio
 import configparser
 import logging
-import multiprocessing
 import os
 import pathlib
 import shlex
@@ -55,90 +55,164 @@ argparser.set_defaults(
     verbose=0,
     cmdfile='-')
 
-_log = logging.getLogger()
+_log = logging.getLogger('LTP')  # pylint: disable=invalid-name
 
 class TestRunner:
     # pylint: disable=too-few-public-methods
+    def __init__(self, suite, tag, cmd):
+        self.suite = suite
+        self.tag = tag
+        self.cmd = cmd
 
-    def __init__(self, config):
-        self.config = config
+        try:
+            self.cfgsection = self.suite.config[self.tag]
+        except configparser.NoSectionError:
+            self.cfgsection = self.suite.config[
+                self.suite.config.defaultsection]
 
-        self.loader = [
-            str(config.getpath(config.default_section, 'loader').resolve())]
-        if self.config.getboolean(config.default_section, 'sgx'):
-            self.loader.append('SGX')
+        self.classname = self.cfgsection.get('junit-classname')
+        self.log = _log.getChild(self.tag)
 
-        self.bindir = str(
-            config.getpath(config.default_section, 'ltproot') / 'testcases/bin')
+        self.stdout = None
+        self.stderr = None
+        self.time = None
 
-    def run_test(self, tag, cmd):
-        # NOTE that this function is run in multiprocessing.Pool, so the return
-        # value is constrained: it should be picklable. Currently we pass a list
-        # of etree.Element()s.
+        self._added_result_with_time = False
 
-        result = XMLTestResultFactory(
-            tag, self.config.get(tag, 'junit-classname'))
 
-        if self.config.getboolean(tag, 'skip', fallback=False):
-            _log.info('%s: SKIP (config)', tag)
-            return 0, [result.skipped('skipped via config')]
+    def _get_subtest_logger(self, subtest):
+        return self.log if subtest is None else self.log.getChild(str(subtest))
 
-        if any(c in cmd for c in ';|&'):
-            # This is a shell command which would spawn multiple processes.
-            # We don't run those in unit tests.
-            _log.info('%s: SKIP (shell command)', tag)
-            return 0, [result.skipped('skipped for invalid shell command')]
+    def _add_result(self, subtest):
+        element = etree.Element('testcase',
+            classname=self.classname,
+            name=('{}/{}'.format(self.tag, subtest)
+                if subtest is not None
+                else self.tag))
 
-        xfail = self.config.getintset(tag, 'xfail', fallback=set())
+        self.suite.add_result(element)
+        self.suite.inc('tests')
 
-        cmd = [*self.loader, *shlex.split(cmd)]
-        timeout = self.config.getfloat(tag, 'timeout')
-        _log.info('%s: starting %r with timeout %d', tag, cmd, timeout)
+        if self.time is not None and subtest is None:
+            assert not self._added_result_with_time
+            self._added_result_with_time = True
+            element.set('time', '{:.3f}'.format(self.time))
+            self.suite.inc('time', self.time, type=float, fmt='.3f')
+
+        if self.stdout is not None:
+            etree.SubElement(element, 'system-out').text = self.stdout
+        if self.stderr is not None:
+            etree.SubElement(element, 'system-err').text = self.stderr
+
+        return element
+
+    def success(self, *, subtest=None):
+        # pylint: disable=redefined-outer-name
+        self._get_subtest_logger(subtest).info('-> PASS')
+        self._add_result(subtest)
+
+    def failure(self, message, *, subtest=None):
+        # pylint: disable=redefined-outer-name
+        self._get_subtest_logger(subtest).warning('-> FAIL (%s)', message)
+        etree.SubElement(self._add_result(subtest), 'failure', message=message)
+        self.suite.inc('failures')
+
+    def error(self, message, *, subtest=None):
+        # pylint: disable=redefined-outer-name
+        self._get_subtest_logger(subtest).error('-> ERROR (%s)', message)
+        etree.SubElement(self._add_result(subtest), 'error').text = message
+        self.suite.inc('errors')
+
+    def skipped(self, message, *, subtest=None):
+        # pylint: disable=redefined-outer-name
+        self._get_subtest_logger(subtest).warning('-> SKIP (%s)', message)
+        etree.SubElement(self._add_result(subtest), 'skipped').text = message
+        self.suite.inc('skipped')
+
+
+    async def _run_cmd(self):
+        cmd = [*self.suite.loader, *shlex.split(self.cmd)]
+        timeout = self.cfgsection.getfloat('timeout')
+        self.log.info('starting %r with timeout %d', cmd, timeout)
         start_time = time.time()
-        proc = subprocess.Popen(  # pylint: disable=subprocess-popen-preexec-fn
-            cmd,
-            cwd=self.bindir,
+
+        # pylint: disable=subprocess-popen-preexec-fn
+        proc = await asyncio.create_subprocess_exec(
+            *cmd,
+            cwd=self.suite.bindir,
             stdout=subprocess.PIPE, stderr=subprocess.PIPE,
             preexec_fn=os.setsid,
             close_fds=True)
+
         try:
-            stdout, stderr = proc.communicate(timeout=timeout)
+            stdout, stderr = await asyncio.wait_for(
+                proc.communicate(), timeout=timeout)
+
         except subprocess.TimeoutExpired as err:
-            _log.warning('%s: -> FAIL (timeout %d s)', tag, timeout)
             if err.stdout is not None:
-                result.stdout = err.stdout.decode()
+                self.stdout = err.stdout.decode()
             if err.stderr is not None:
-                result.stderr = err.stderr.decode()
-            return timeout, [
-                result.error('Timed out after {} s.'.format(timeout))]
+                self.stderr = err.stderr.decode()
+            self.error('Timed out after {} s.')
+            return
+
         finally:
             try:
                 os.killpg(os.getpgid(proc.pid), signal.SIGKILL)
             except ProcessLookupError:
                 pass
 
-        cmd_time = time.time() - start_time
+        self.time = time.time() - start_time
 
         assert proc.pid is not None
-        _log.debug('%s: finished pid=%d returncode=%d stdout=%r',
-            tag, proc.pid, proc.returncode, stdout)
+        self.log.debug('finished pid=%d returncode=%d stdout=%r',
+            proc.pid, proc.returncode, stdout)
         if stderr:
-            _log.info('%s: stderr: %r', tag, stderr)
+            self.log.info('stderr=%r', stderr)
 
-        result.stdout, result.stderr = stdout.decode(), stderr.decode()
-        partial = list(
-            self._parse_test_output(tag, result.stdout, result, xfail))
-        if not partial:
-            partial.append(result.error('binary did not provide any results'))
-        return cmd_time, partial
+        self.stdout, self.stderr = stdout.decode(), stderr.decode()
 
-    @staticmethod
-    def _parse_test_output(tag, stdout, result, xfail):
+        return proc.returncode
+
+    async def execute(self):
+        if self.cfgsection.getboolean('skip', fallback=False):
+            self.skipped('skipped via config')
+            return
+
+        if any(c in self.cmd for c in ';|&'):
+            # This is a shell command which would spawn multiple processes.
+            # We don't run those in unit tests.
+            if 'must-pass' in self.cfgsection:
+                self.error('invalid shell command with must-pass')
+            else:
+                self.skipped('invalid shell command')
+            return
+
+        async with self.suite.semaphore:
+            returncode = await self._run_cmd()
+
+        try:
+            must_pass = self.cfgsection.getset('must-pass')
+        except configparser.NoOptionError:
+            # must-pass is not set -- report single result
+            if returncode == 0:
+                self.success()
+            else:
+                self.failure('failed')
+            return
+        else:
+            self._parse_test_output(must_pass)
+
+    def _parse_test_output(self, must_pass):
         # pylint: disable=too-many-branches
 
+        not_found = must_pass.copy()
+        maybe_unneeded_must_pass = True
+        has_result = False
+
         subtest = 0
-        for line in stdout.split('\n'):
-            _log.debug('%s: <- %r', tag, line)
+        for line in self.stdout.split('\n'):
+            self.log.debug('<- %r', line)
 
             if line == 'Summary:':
                 break
@@ -159,106 +233,90 @@ class TestRunner:
             else:
                 subtest += 1
 
-            result.name = '{}/{}'.format(tag, subtest)
+            try:
+                not_found.remove(subtest)
+            except KeyError:
+                # subtest is not in must-pass
+                maybe_unneeded_must_pass = False
 
             if 'TPASS' in line or 'PASS:' in line:
-                if subtest in xfail:
-                    _log.warning('%s/%d: -> XFAIL/PASS', tag, subtest)
-                    yield result.skipped(line)
+                self.success(subtest=subtest)
+                has_result = True
+                continue
+
+            if any(t in line for t in (
+                    'TFAIL', 'TCONF', 'CONF:', 'TBROK', 'BROK:')):
+                if subtest in must_pass:
+                    self.failure(line, subtest=subtest)
+                    maybe_unneeded_must_pass = False
                 else:
-                    _log.info('%s/%d: -> PASS', tag, subtest)
-                    yield result.success()
+                    self.skipped(line, subtest=subtest)
+                has_result = True
                 continue
 
-            if 'TFAIL' in line:
-                if subtest in xfail:
-                    _log.info('%s/%d: -> XFAIL', tag, subtest)
-                    yield result.skipped(line)
-                else:
-                    _log.warning('%s/%d: -> FAIL', tag, subtest)
-                    yield result.failure(line)
-                continue
+            self.error(line, subtest=subtest)
 
-            if any(t in line for t in ('TCONF', 'CONF:', 'TBROK', 'BROK:')):
-                _log.warning('%s/%d: -> SKIP', tag, subtest)
-                yield result.skipped(line)
-                continue
+        for subtest in sorted(not_found):
+            self.failure('not even attempted', subtest=subtest)
 
-            _log.debug('%s/%d: -> ERROR', tag, subtest)
-            yield result.error(line)
+        if maybe_unneeded_must_pass and not not_found:
+            # all subtests passed and must-pass specified exactly all subtests
+            self.error('must-pass is unneeded, remove it from config')
 
-class XMLTestResultFactory:
-    def __init__(self, name, classname, stdout=None, stderr=None):
-        self.name = name
-        self.classname = classname
-        self.stdout = stdout
-        self.stderr = stderr
+        if not has_result:
+            self.error('binary did not provide any subtests')
 
-    def _get_new_element(self):
-        element = etree.Element('testcase',
-            name=self.name, classname=self.classname)
-        if self.stdout is not None:
-            etree.SubElement(element, 'system-out').text = self.stdout
-        if self.stderr is not None:
-            etree.SubElement(element, 'system-err').text = self.stderr
-        return element
+class TestSuite:
+    def __init__(self, config):
+        self.config = config
 
-    def success(self):
-        return self._get_new_element()
+        self.loader = [
+            str(config.getpath(config.default_section, 'loader').resolve())]
+        if self.config.getboolean(config.default_section, 'sgx'):
+            self.loader.append('SGX')
 
-    def failure(self, message):
-        element = self._get_new_element()
-        etree.SubElement(element, 'failure', message=message)
-        return element
+        self.bindir = str(
+            config.getpath(config.default_section, 'ltproot') / 'testcases/bin')
 
-    def error(self, message):
-        element = self._get_new_element()
-        etree.SubElement(element, 'error').text = message
-        return element
+        # Running parallel tests under SGX is risky, see README.
+        # However, if user wanted to do that, we shouldn't stand in the way,
+        # just issue a warning.
+        has_sgx = config.getboolean(config.default_section, 'sgx')
+        processes = config.getint(config.default_section, 'jobs',
+            fallback=(1 if has_sgx else None))
+        if has_sgx and processes != 1:
+            _log.warning('WARNING: SGX is enabled and jobs = %d (!= 1);'
+                ' expect stability issues', processes)
 
-    def skipped(self, message):
-        element = self._get_new_element()
-        etree.SubElement(element, 'skipped').text = message
-        return element
+        self.semaphore = asyncio.BoundedSemaphore(processes)
+        self.queue = []
+        self.xml = etree.Element('testsuite')
+        self.time = 0
 
-class XMLReport:
-    def __init__(self):
-        self.root = etree.Element('testsuite')
+    def add_test(self, tag, cmd):
+        self.queue.append(TestRunner(self, tag, cmd))
+
+    def add_result(self, element):
+        self.xml.append(element)
 
     def inc(self, accumulator, value=1, *, type=int, fmt=''):
         # pylint: disable=redefined-builtin
-        self.root.set(accumulator,
-            format(type(self.root.get(accumulator, 0)) + value, fmt))
+        self.xml.set(accumulator,
+            format(type(self.xml.get(accumulator, 0)) + value, fmt))
 
-    def add_result(self, element):
-        self.root.append(element)
-        self.inc('tests')
-        self.inc('failures', len(element.findall('failure')))
-        self.inc('skipped', len(element.findall('skipped')))
-        self.inc('errors', len(element.findall('error')))
 
-    def write(self, stream):
-        etree.ElementTree(self.root).write(stream)
+    def write_report(self, stream):
+        etree.ElementTree(self.xml).write(stream)
 
-class LTPConfigParser(configparser.ConfigParser):
-    # ConfigParser raises NoSectionError for nonexistent sections, even when the
-    # option value is set in [DEFAULT]. That's documented and expected, but not
-    # what we want. We want to return the default value even for nonexistent
-    # sections.
-    # pylint: disable=too-many-ancestors
+    async def execute(self):
+        await asyncio.gather(*(runner.execute() for runner in self.queue))
 
-    def get(self, section, *args, **kwds):
-        # pylint: disable=arguments-differ
-        try:
-            return super().get(section, *args, **kwds)
-        except configparser.NoSectionError:
-            return super().get(self.default_section, *args, **kwds)
 
 def _getintset(value):
     return set(int(i) for i in value.strip().split())
 
 def load_config(file):
-    config = LTPConfigParser(
+    config = configparser.ConfigParser(
         converters={
             'path': pathlib.Path,
             'intset': _getintset,
@@ -276,9 +334,10 @@ def load_config(file):
 
     return config
 
-
 def main(args=None):
-    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.WARNING)
+    logging.basicConfig(
+        format='%(asctime)s %(name)s: %(message)s',
+        level=logging.WARNING)
     args = argparser.parse_args(args)
     _log.setLevel(_log.level - args.verbose * 10)
 
@@ -287,35 +346,17 @@ def main(args=None):
         key, value = token.split('=', maxsplit=1)
         config[config.default_section][key] = value
 
-    tests = []
+    suite = TestSuite(config)
     with args.cmdfile as file:
         for line in file:
             if line[0] in '\n#':
                 continue
             tag, cmd = line.strip().split(maxsplit=1)
-            tests.append((tag, cmd))
+            suite.add_test(tag, cmd)
 
-    report = XMLReport()
-    runner = TestRunner(config)
-
-    # Running parallel tests under SGX is risky, see README. However, if user
-    # wanted to do that, we shouldn't stand in the way, just issue a warning.
-    has_sgx = config.getboolean(config.default_section, 'sgx')
-    processes = config.getint(config.default_section, 'jobs',
-        fallback=(1 if has_sgx else None))
-    if has_sgx and processes != 1:
-        _log.warning('WARNING: SGX is enabled and jobs = %d (!= 1);'
-            ' expect stability issues', processes)
-
-    total_time = 0
-    with multiprocessing.Pool(processes) as pool:
-        for cmd_time, partial in pool.starmap(runner.run_test, tests):
-            for element in partial:
-                report.add_result(element)
-            total_time += cmd_time
-
-    report.inc('time', total_time, type=float, fmt='.3f')
-    report.write(sys.stdout.buffer)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(suite.execute())
+    suite.write_report(sys.stdout.buffer)
 
 if __name__ == '__main__':
     main()
