ad2469365f35450b66add1c295773949cdd55fc2
Chia-Che Tsai <chiache@tamu.edu>
2019-09-03 15:57:55 -0500

    fixup! fixup! fixup! [Apps] Rewrite Python example

--- python/run-tests.sh
+++ python/run-tests.sh
@@ -2,26 +2,24 @@
 
 set -e
 
-echo "\n\nRunning helloworld.py:"
+echo -e "\n\nRunning helloworld.py:"
 ./pal_loader python.manifest scripts/helloworld.py > OUTPUT
 grep -q "Hello World" OUTPUT && echo "[ SUCCESS ]"
 rm OUTPUT
 
-echo "\n\nRunning fibonacci.py:"
-./pal_loader python.manifest scripts/fibonacci.py  > OUTPUT
+echo -e "\n\nRunning fibonacci.py:"
+./pal_loader python.manifest scripts/fibonacci.py > OUTPUT
 grep -q "fib2              55" OUTPUT && echo "[ SUCCESS ]"
 rm OUTPUT
 
-echo "\n\nRunning test-numpy.py:"
+echo -e "\n\nRunning test-numpy.py:"
 ./pal_loader python.manifest scripts/test-numpy.py > OUTPUT
 grep -q "maxint: " OUTPUT && echo "[ SUCCESS ]"
-cat OUTPUT
 rm OUTPUT
 
-echo "\n\nRunning test-scipy.py:"
+echo -e "\n\nRunning test-scipy.py:"
 ./pal_loader python.manifest scripts/test-scipy.py > OUTPUT
 grep -q "cholesky: " OUTPUT && echo "[ SUCCESS ]"
-cat OUTPUT
 rm OUTPUT
 
 ./web-test.sh
