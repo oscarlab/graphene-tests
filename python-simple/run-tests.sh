4e62c303d408b41982fe95c10e7de8828a1a8174
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2019-11-26 12:16:16 -0800

    fixup! Rewrite Python example

--- python-simple/run-tests.sh
+++ python-simple/run-tests.sh
@@ -26,5 +26,3 @@ echo >> OUTPUT2  # include newline since wget doesn't add it
 diff -q OUTPUT1 OUTPUT2 && echo "[ Success 3/3 ]"
 kill `cat server.PID`
 rm -f OUTPUT1 OUTPUT2 server.PID
-
-sleep 5  # to make sure Python processes finish under Graphene-SGX
