595b82e8b5d3e3a85862f40f0485ebc895e6b569
borysp <borysp@invisiblethingslab.com>
2020-01-10 23:14:16 +0100

    [LTP] Re-enable test cases from ltp-bug-1023.cfg and add ltp-bug-1248.cfg

--- ltp/makevars.mk
+++ ltp/makevars.mk
@@ -3,4 +3,4 @@ SRCDIR = $(ROOTDIR)/src
 BUILDDIR = $(ROOTDIR)/opt/ltp
 TESTCASEDIR = $(BUILDDIR)/testcases/bin
 LTPSCENARIO = $(BUILDDIR)/runtest/syscalls
-RUNLTPOPTS = -c ltp-bug-1023.cfg
+RUNLTPOPTS = -c ltp-bug-1248.cfg
