3af5de624f397a890d4bba7409e0f6ea19804cbd
Eduardo Rodriguez <erodrig@us.ibm.com@eduardos-mbp-2.austin.ibm.com>
2020-02-14 09:27:51 -0600

    Adding .gitignore and accepting port as parameter

--- nodejs-express-server/helloworld.js
+++ nodejs-express-server/helloworld.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const args = process.argv;
+
+if (args == null || args.length < 3 || args.length > 3){
+  console.log('The only argument required is the port number.')
+  return;
+}
+
+const port = process.argv[2];
+
+if (isNaN(port)){
+  console.log('Port argument is not a number.')
+  return;
+}
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
