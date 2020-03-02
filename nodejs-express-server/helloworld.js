e6f081e0a90e43bd5a48e2582ccd14d276a35f54
Eduardo Rodriguez <erodrig@us.ibm.com>
2020-03-02 11:26:41 -0600

    Fixing .gitignore, adding white spates in helloworld.js, adding ERROR prefix to log output.

--- nodejs-express-server/helloworld.js
+++ nodejs-express-server/helloworld.js
@@ -2,15 +2,15 @@ const express = require('express')
 const app = express()
 const args = process.argv;
 
-if (args == null || args.length != 3){
-  console.log('The only argument required is the port number.')
+if (args == null || args.length != 3) {
+  console.log('Error: The only argument required is the port number.')
   return;
 }
 
 const port = process.argv[2];
 
-if (isNaN(port)){
-  console.log('Port argument is not a number.')
+if (isNaN(port)) {
+  console.log('Error: Port argument is not a number.')
   return;
 }
 
